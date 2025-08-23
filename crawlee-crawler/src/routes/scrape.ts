51fb265ffd71400b44fc7a522f5193445404000b
Shun Yuan <tanshunyuan99@gmail.com>
2025-08-23 14:52:57 +0800

    chore: add basic server

---
+++ crawlee-crawler/src/routes/scrape.ts
@@ -0,0 +1,47 @@
+// https://crawlee.dev/js/docs/guides/running-in-web-server
+
+import { CheerioCrawler, log } from "crawlee";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { randomUUID } from "node:crypto";
+import z from "zod";
+
+export const scrapeSchema = z.object({
+  url: z.url().min(1),
+});
+
+// We will bind an HTTP response that we want to send to the Request.uniqueKey
+const requestsToResponses = new Map();
+
+export const scrapeHandler = async (
+  req: FastifyRequest<{ Body: z.infer<typeof scrapeSchema> }>,
+  res: FastifyReply,
+) => {
+  try {
+    const crawler = new CheerioCrawler({
+      keepAlive: true,
+      requestHandler: async ({ request, $ }) => {
+        const title = $("title").text();
+        // We will send the response here later
+        log.info(`Page title: ${title} on ${request.url}`);
+
+        res
+          .code(200)
+          .header('content-type', 'application/json')
+          .send(JSON.stringify({title}))
+
+        // We can delete the response from the map now to free up memory
+        requestsToResponses.delete(request.uniqueKey);
+      },
+    });
+
+    const crawleeRequest = { url: req.body.url, uniqueKey: randomUUID() };
+    requestsToResponses.set(crawleeRequest.uniqueKey, res);
+    await crawler.addRequests([crawleeRequest]);
+
+    await crawler.run()
+
+  } catch (error) {
+    req.log.error(error);
+    res.code(500).send({ error: (error as Error).message });
+  }
+};
---
+++ crawlee-crawler/src/server.ts
@@ -0,0 +1,49 @@
+import fastify from "fastify";
+import cors from '@fastify/cors'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { scrapeHandler, scrapeSchema } from "./routes/scrape.js";
+
+
+const port = parseInt('8000');
+const host = `localhost`;
+
+const server = fastify({
+  logger: {
+    level: "debug",
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+      },
+    },
+  },
+})
+  .register(cors, {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+  })
+
+server.setValidatorCompiler(validatorCompiler);
+server.setSerializerCompiler(serializerCompiler);
+
+server.withTypeProvider<ZodTypeProvider>().route({
+  method: "POST",
+  url: "/scrape",
+  schema: {
+    body: scrapeSchema,
+  },
+  handler: scrapeHandler,
+});
+
+server.listen({ host, port }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
