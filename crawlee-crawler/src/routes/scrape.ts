6f99d9e9ef3b712c16a305a659deacde47ebc0fe
Shun Yuan <tanshunyuan99@gmail.com>
2025-08-23 16:27:19 +0800

    feat: add crawl regex

--- crawlee-crawler/src/routes/scrape.ts
+++ crawlee-crawler/src/routes/scrape.ts
@@ -1,45 +1,64 @@
 // https://crawlee.dev/js/docs/guides/running-in-web-server
 
-import { CheerioCrawler, log } from "crawlee";
+import { CheerioCrawler, EnqueueStrategy, log } from "crawlee";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { randomUUID } from "node:crypto";
 import z from "zod";
 
 export const scrapeSchema = z.object({
-  url: z.string().url().min(1)
+  url: z.string().url().min(1),
 });
 
-// We will bind an HTTP response that we want to send to the Request.uniqueKey
-const requestsToResponses = new Map();
+const SOCIAL_MEDIA_DOMAINS = [
+  "facebook.com",
+  "twitter.com",
+  "x.com",
+  "linkedin.com",
+  "instagram.com",
+  "pinterest.com",
+  "tiktok.com",
+  "snapchat.com",
+  "reddit.com",
+];
+
+const COMMON_PAGES = [
+  /.*about.*/i,
+  /.*contact.*/i,
+  /.*service.*/i,
+  /.*home.*/i,
+  /.*company.*/i,
+  /.*team.*/i,
+  /.*mission.*/i,
+  /.*history.*/i,
+  /.*overview.*/i,
+];
 
 export const scrapeHandler = async (
   req: FastifyRequest<{ Body: z.infer<typeof scrapeSchema> }>,
   res: FastifyReply,
 ) => {
   try {
+    const results: string[] = [];
     const crawler = new CheerioCrawler({
-      keepAlive: true,
-      requestHandler: async ({ request, $ }) => {
+      // keepAlive: true,
+      requestHandler: async ({ request, $, enqueueLinks }) => {
         const title = $("title").text();
-        // We will send the response here later
         log.info(`Page title: ${title} on ${request.url}`);
-
-        res
-          .code(200)
-          .header('content-type', 'application/json')
-          .send(JSON.stringify({title}))
-
-        // We can delete the response from the map now to free up memory
-        requestsToResponses.delete(request.uniqueKey);
+        results.push(title);
+        await enqueueLinks({
+          regexps: [...COMMON_PAGES],
+          exclude: [...SOCIAL_MEDIA_DOMAINS],
+          strategy: EnqueueStrategy.SameHostname,
+        });
       },
+      maxRequestsPerCrawl: 10,
     });
 
-    const crawleeRequest = { url: req.body.url, uniqueKey: randomUUID() };
-    requestsToResponses.set(crawleeRequest.uniqueKey, res);
-    await crawler.addRequests([crawleeRequest]);
-
-    await crawler.run()
-
+    await crawler.run([req.body.url]);
+    res
+      .code(200)
+      .header("content-type", "application/json")
+      .send(JSON.stringify(results));
   } catch (error) {
     req.log.error(error);
     res.code(500).send({ error: (error as Error).message });
