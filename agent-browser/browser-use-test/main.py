322c2d43b1e55d8b29d42e11331290ea60da96b7
Shun Yuan <tanshunyuan99@gmail.com>
2026-03-26 21:17:38 +0800

    chore: cannot rmb

---
+++ agent-browser/browser-use-test/main.py
@@ -0,0 +1,21 @@
+from browser_use import Agent, Browser, ChatBrowserUse
+# from browser_use import ChatGoogle  # ChatGoogle(model='gemini-3-flash-preview')
+# from browser_use import ChatAnthropic  # ChatAnthropic(model='claude-sonnet-4-6')
+import asyncio
+
+async def main():
+    browser = Browser(
+        # use_cloud=True,  # Use a stealth browser on Browser Use Cloud
+    )
+
+    agent = Agent(
+        task="Find the number of stars of the browser-use repo",
+        llm=ChatBrowserUse(),
+        # llm=ChatGoogle(model='gemini-3-flash-preview'),
+        # llm=ChatAnthropic(model='claude-sonnet-4-6'),
+        browser=browser,
+    )
+    await agent.run()
+
+if __name__ == "__main__":
+    asyncio.run(main())
